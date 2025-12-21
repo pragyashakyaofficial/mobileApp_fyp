ae9a85cd2896ead2f3ba99a491fc27809f75fb46
Suraj Shakya <surajshakyaofficial@gmail.com>
2025-12-21 23:01:30 +0545

    path alishing

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@app': './src/app',
+          '@api': './src/api',
+          '@features': './src/features',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@hooks': './src/hooks',
+          '@services': './src/services',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@assets': './src/assets',
+          '@types': './src/types',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
